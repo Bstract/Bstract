a4c973da50682c1f44d773e25c47134dd47fdc0c
Sam Mead <samjamead@gmail.com>
2021-11-17 19:09:43 +0000

    Light modes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,11 +7,26 @@ jQuery(document).ready(function($){
   fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=12&collection=ice-dance-1', options)
     .then(response => response.json())
     .then(response => $.each(response.assets, function(i, item) {
-        console.log(item.token_id);
         $('#ice-dance-collection').append(
-          '<li><img src="' + item.image_url + '" alt="@Bstract artwork on OpenSea" /></li>'
+          '<li>' +
+            '<img src="' + item.image_url + '" alt="@Bstract artwork on OpenSea" />' +
+            '<h2>' + item.name + '</h2>' +
+            '<p><a href="' + item.permalink + '" target="blank">Buy on OpenSea</a></p>' +
+          '</li>'
         );
     }))
     .catch(err => console.error(err));
+
+  $('#lightswitch').on('click', function(){
+    console.log("Lightswitch clicked!");
+    var mode = $('#light-mode-label');
+    if (mode.text() == "Light") {
+      mode.text("Dark");
+    } else {
+      mode.text("Light");
+    }
+    $('.dark-mode-logo').toggle();
+    $('body').toggleClass('light-mode');
+  });
 
 });
