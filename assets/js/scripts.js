acf04e888d3c6e3af463db1deaa8692a7bf4f9a0
Sam Mead <samjamead@gmail.com>
2021-11-22 15:33:36 +0000

    Good direction

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,33 +11,22 @@ jQuery(document).ready(function($){
     .then(response => $.each(response.assets, function(i, item) {
         $('#ice-dance-collection').append(
           '<li>' +
-            '<img src="' + item.image_url + '" alt="@Bstract artwork on OpenSea" />' +
-            '<div class="card-info">' +
-              '<h2>' + item.name + '</h2>' +
-              '<p><a class="button icon-button" href="' + item.permalink + '" target="blank">Buy on OpenSea<span class="icon-wrapper"><i id="lightswitch" class="fas fa-arrow-right"></i></span></a></p>' +
-            '</div>' +
+            '<a href="' + item.permalink + '" target="_blank">' +
+              '<img src="' + item.image_url + '" alt="@Bstract artwork on OpenSea" />' +
+              '<div class="card-info">' +
+                '<h2>' + item.name + '</h2>' +
+              '</div>' +
+            '</a>' +
           '</li>'
         );
         // Limit number of pieces listed
-        return i<7;
+        return i<11;
     }))
     .catch(err => console.error(err));
 
-  $('#lightswitch').on('click', function(){
-    console.log("Lightswitch clicked!");
-    var mode = $('#light-mode-label');
-    if (mode.text() == "Light") {
-      mode.text("Dark");
-    } else {
-      mode.text("Light");
-    }
-    $('.dark-mode-logo').toggle();
-    $('body').toggleClass('light-mode');
-  });
-
   // Open the menu – thanks Jonathon Suh
   var $hamburger = $('.hamburger');
-  $hamburger.on("click", function(){
+  $('.menu-toggle').on("click", function(){
     $hamburger.toggleClass("is-active");
     $('.nav-container').toggleClass("menu-open");
   });
